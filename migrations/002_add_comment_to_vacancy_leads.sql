c6b189382695df11b91db01de55478b2672cd978
Allender45 <crash-em@mail.ru>
2026-08-25 16:19:54 +0500

    083-Хотфикс миграций.

--- migrations/002_add_comment_to_vacancy_leads.sql
+++ migrations/002_add_comment_to_vacancy_leads.sql
@@ -1,1 +1,6 @@
-ALTER TABLE vacancy_leads ADD COLUMN IF NOT EXISTS comment TEXT;
+DO $$
+    BEGIN
+        IF EXISTS (SELECT FROM information_schema.tables WHERE table_name = 'vacancy_leads') THEN
+            ALTER TABLE vacancy_leads ADD COLUMN IF NOT EXISTS comment TEXT;
+        END IF;
+    END $$;
