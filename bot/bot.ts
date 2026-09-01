3b2ae8bfd9f843aff1c28ae23113a6d4a708c347
Allender45 <crash-em@mail.ru>
2026-09-01 14:52:05 +0500

    092-Фикс тг бота для прода.

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,5 +1,10 @@
 import { Telegraf } from 'telegraf';
+import { SocksProxyAgent } from 'socks-proxy-agent';
 import { BOT_TOKEN } from './config';
 import { BotContext } from './middlewares/resolveUser';
 
-export const bot = new Telegraf<BotContext>(BOT_TOKEN);
+const agent = process.env.TELEGRAM_SOCKS_PROXY
+    ? new SocksProxyAgent(process.env.TELEGRAM_SOCKS_PROXY)
+    : undefined;
+
+export const bot = new Telegraf<BotContext>(BOT_TOKEN, agent ? { telegram: { agent } } : undefined);
